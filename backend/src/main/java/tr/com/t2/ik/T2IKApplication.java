2972e6ba4e702c599a9d2681ce2de325cf38fe25
Selin Çabuk <selincbk13@gmail.com>
2023-07-31 09:50:29 +0300

    Revert "database filled"
    This reverts commit 4988b49703b2c99bd2584f2773867e6211fb45aa.

--- backend/src/main/java/tr/com/t2/ik/T2IKApplication.java
+++ backend/src/main/java/tr/com/t2/ik/T2IKApplication.java
@@ -13,9 +13,6 @@ import tr.com.t2.ik.repository.RoleRepository;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-
 
 @SpringBootApplication
 public class T2IKApplication {
@@ -35,65 +32,27 @@ public class T2IKApplication {
             user.setName("ROLE_USER");
 
             roleRepository.saveAll(Arrays.asList(admin,user));
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
 
             Personnel mete = new Personnel();
             mete.setUsername("metehan.danaci");
             mete.setPassword(new BCryptPasswordEncoder().encode("mete"));
             mete.setRoles(new HashSet<>(Arrays.asList(admin, user)));
-            mete.setActivity("Aktif");
-            mete.setFirstname("Metehan");
-            mete.setLastname("Danacı");
-            String metehanStringBirthday = "1984-06-02";
-            Date metehanBirthday = formatter.parse(metehanStringBirthday);
-            mete.setBirthday(metehanBirthday);
-            String metehanStringStartDate = "2010-07-02";
-            Date metehanStartDate = formatter.parse(metehanStringStartDate);
-            mete.setStartDate(metehanStartDate);
-
 
             Personnel tan = new Personnel();
             tan.setUsername("tan.apaydin");
             tan.setPassword(new BCryptPasswordEncoder().encode("tan"));
             tan.setRoles(new HashSet<>(Collections.singletonList(user)));
-            tan.setActivity("Pasif");
-            tan.setFirstname("Tan");
-            tan.setLastname("Apaydın");
-            String tanStringBirthday = "1987-12-31";
-            Date tanBirthday = formatter.parse(tanStringBirthday);
-            tan.setBirthday(tanBirthday);
-            String tanStringStartDate = "2011-12-01";
-            Date tanStartDate = formatter.parse(tanStringStartDate);
-            tan.setStartDate(tanStartDate);
 
             Personnel selin = new Personnel();
             selin.setUsername("selin");
             selin.setPassword(new BCryptPasswordEncoder().encode("sel"));
             selin.setRoles(new HashSet<>(Collections.singletonList(user)));
-            selin.setActivity("Aktif");
-            selin.setFirstname("Selin");
-            selin.setLastname("Çabuk");
-            String selinStringBirthday = "2000-05-12";
-            Date selinBirthday = formatter.parse(selinStringBirthday);
-            selin.setBirthday(selinBirthday);
-            String selinStringStartDate = "2023-07-10";
-            Date selinStartDate = formatter.parse(selinStringStartDate);
-            selin.setStartDate(selinStartDate);
-
 
             Personnel ceren = new Personnel();
             ceren.setUsername("ceren");
             ceren.setPassword(new BCryptPasswordEncoder().encode("cero"));
             ceren.setRoles(new HashSet<>(Collections.singletonList(user)));
-            ceren.setActivity("Aktif");
-            ceren.setFirstname("Ceren");
-            ceren.setLastname("Akyar");
-            String cerenStringBirthday = "2002-05-01";
-            Date cerenBirthday = formatter.parse(cerenStringBirthday);
-            ceren.setBirthday(cerenBirthday);
-            String cerenStringStartDate = "2023-07-10";
-            Date cerenStartDate = formatter.parse(cerenStringStartDate);
-            ceren.setStartDate(cerenStartDate);
 
             personnelRepository.saveAll(Arrays.asList(mete,tan,selin,ceren));
         };
