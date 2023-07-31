72cc5954f58745b6f0fa74ca0599c843e3cd3c2d
CerenAkyr <cerenakyar@gmail.com>
2023-07-31 09:44:35 +0300

    Changes

--- backend/src/main/java/tr/com/t2/ik/ws/UserController.java
+++ backend/src/main/java/tr/com/t2/ik/ws/UserController.java
@@ -2,10 +2,7 @@ package tr.com.t2.ik.ws;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tr.com.t2.ik.model.Personnel;
 import tr.com.t2.ik.repository.PersonnelRepository;
 import tr.com.t2.ik.ws.dto.PersonnelResponseDTO;
@@ -14,17 +11,27 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-@PreAuthorize("hasRole('ROLE_USER')")
+//@PreAuthorize("hasRole('ROLE_USER')")
 public class UserController {
 
     @Autowired
     private PersonnelRepository personnelRepository;
 
+    @CrossOrigin
     @GetMapping
     public String getMethod() {
-        return "User Area, Welcome";
+        Iterable<Personnel> personnelOptional = personnelRepository.findAll();
+        if (personnelOptional.isPresent()) {
+            return PersonnelResponseDTO
+                    .builder()
+                    .username(personnelOptional.get().getUsername())
+                    .roles(personnelOptional.get().getRoles())
+                    .build();
+        }
+        return null;
     }
 
+    @CrossOrigin
     @GetMapping
     @RequestMapping("/{username}")
     public PersonnelResponseDTO getPersonnel(@PathVariable("username") String username) {
