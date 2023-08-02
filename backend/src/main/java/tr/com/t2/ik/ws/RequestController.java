0bb53f622aec7bc0fcc38897386fa3006878fc8c
Selin Çabuk <selincbk13@gmail.com>
2023-08-02 09:25:15 +0300

    xx

--- backend/src/main/java/tr/com/t2/ik/ws/RequestController.java
+++ backend/src/main/java/tr/com/t2/ik/ws/RequestController.java
@@ -1,24 +1,15 @@
 package tr.com.t2.ik.ws;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import tr.com.t2.ik.model.OffDayRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
+import tr.com.t2.ik.model.OffDayRequest;
 import tr.com.t2.ik.repository.RequestRepository;
 import tr.com.t2.ik.security.JwtTokenUtil;
 
-
-
-
 @RestController
-
-
+@RequestMapping("/api/off")
 public class RequestController {
 
     @Autowired
@@ -30,39 +21,30 @@ public class RequestController {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
-
-    @PostMapping("/api/off")
     @CrossOrigin
-    public ResponseEntity<?> addOffDays(@RequestBody OffDayRequest offDayRequest) {
+    @PostMapping
+    public ResponseEntity<String> addOffRequest(@RequestBody OffDayRequest offRequest) {
+       /* try {
+            String token = offRequest.getToken();
+            boolean isTokenValid = jwtTokenUtil.validateToken(token);
 
-        try {
-            // Burada veritabanına kaydetme işlemi
-            // İlgili entity sınıfınızın (ExcuseLeave) veritabanına kaydeden bir repository (excuseLeaveRepository) kullanılmalıdır.
-            // Örnek olarak:
+            if (!isTokenValid) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Geçersiz token");
+            }
 
-            OffDayRequest receivedOffDayRequest = new OffDayRequest();
+         
+            OffDayRequest receivedOffRequest = new OffDayRequest();
+            receivedOffRequest.setExcuseType(offRequest.getExcuseType());
+            receivedOffRequest.setExcuseStartDate(offRequest.getExcuseStartDate());
+            receivedOffRequest.setExcuseEndDate(offRequest.getExcuseEndDate());
+            receivedOffRequest.setDescription(offRequest.getDescription());
 
-            receivedOffDayRequest.setRequestId(offDayRequest.getRequestId());
-            receivedOffDayRequest.setRequestStatus(offDayRequest.getRequestStatus());
-            receivedOffDayRequest.setExcuseCreateDate(offDayRequest.getExcuseCreateDate());
-            receivedOffDayRequest.setExcuseStartDate(offDayRequest.getExcuseStartDate());
-            receivedOffDayRequest.setExcuseEndDate(offDayRequest.getExcuseEndDate());
-            receivedOffDayRequest.setExcuseType(offDayRequest.getExcuseType());
-            receivedOffDayRequest.setDescription(offDayRequest.getDescription());
-            receivedOffDayRequest.setApprovedBy(offDayRequest.getApprovedBy());
-            receivedOffDayRequest.setUpdatedBy(offDayRequest.getUpdatedBy());
-            receivedOffDayRequest.setUpdateDay(offDayRequest.getUpdateDay());
+            requestRepository.save(receivedOffRequest); // Veritabanına kayıt işlemi
 
-            // Veritabanına kayıt işlemi
-            requestRepository.save(receivedOffDayRequest);
-
-            // Başarılı yanıt döndür
             return ResponseEntity.ok("İzin isteği başarıyla kaydedildi.");
         } catch (Exception e) {
-            // Hata durumunda hata yanıtı döndür
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("İzin isteği kaydedilirken bir hata oluştu.");
-        }
-
-
+        }*/
+        return null;
     }
 }
