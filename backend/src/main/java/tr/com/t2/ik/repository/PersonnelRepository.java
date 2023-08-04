cd6b9c3804fdd15ad69646bcd8aaf17900499b50
CerenAkyr <cerenakyar@gmail.com>
2023-08-04 15:21:45 +0300

    Update PersonnelRepository.java

--- backend/src/main/java/tr/com/t2/ik/repository/PersonnelRepository.java
+++ backend/src/main/java/tr/com/t2/ik/repository/PersonnelRepository.java
@@ -1,13 +1,11 @@
 package tr.com.t2.ik.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import tr.com.t2.ik.model.Personnel;
-import tr.com.t2.ik.ws.dto.PersonnelDto;
-
-import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PersonnelRepository extends CrudRepository<Personnel, String> {
     Optional<Personnel> findByUsername(String username);
 }
